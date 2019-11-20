b3453e7312ac077841d6ed05c3ad549e62241796
terminalforlife <terminalforlife@yahoo.com>
2019-11-20 15:53:24 +0000

    Add ASCII-art logo for TFL (Learn Linux)

---
+++ TFL_Logo (ASCII).sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while read -r; do
+	printf "\033[1;31m%s\033[0m\n" "$REPLY"
+done <<'EOF'
+
+                       ERMIN
+     ITERNINALTERMINALTMINALA
+    MTERMINALTERMINALTR __  TL
+   RL ______________ E /  /| ET
+  EA /____   ______/| /  / / RE
+  TN |___/  /|______//__/_/  MR
+   LI   /  / / /  _______/| IM
+    AM /__/ / /  /|______|/ NI
+    NR |__|/ /  /_/__/_/   AN
+     IE     /  _______/| TLA
+      MTLA /  /|______|/ EL
+       RN /  / /  / /   RT
+      EI /  / /  / / NIME
+     TM /__/ /  / / AIMR
+     LR |__|/  / / LNALTER
+      AE   /  / / TERMINALM
+       NT /  /_/________  TI
+      IL /_____________/| EN
+      MA |_____________|/ RA
+       RN                ML
+        EIMRETLANIMRETLANT
+         TLANIMRETLANIMRE
+
+EOF
