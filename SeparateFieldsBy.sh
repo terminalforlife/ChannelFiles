945a120355687b7ef96d9a90435bd6170579758e
terminalforlife <terminalforlife@yahoo.com>
2019-11-27 18:57:35 +0000

    Add useful function with demonstration
    See header comments.

---
+++ SeparateFieldsBy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------------------
+# Project Name      - LearnLinux/SeparateFieldsBy.sh
+# Started On        - Wed 27 Nov 18:56:46 GMT 2019
+# Last Change       - Wed 27 Nov 18:57:29 GMT 2019
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#----------------------------------------------------------------------------------
+# Posted to Reddit on 'r/bash' in an effort to help someone with their query.
+#----------------------------------------------------------------------------------
+
+FieldOut(){ # Usage: FieldOut [SEPARATOR] [FIELD_1] [FIELD_2] ...
+	local CurField Sep=$1
+	shift
+
+	local FirstField='true'
+	for CurField in "$@"; {
+		if [ ${FirstField:-false} == 'true' ]; then
+			# It's the first field, so omit separator.
+			unset FirstField
+			printf "%s " "$CurField"
+		else
+			# No separator at the begnining or end of line.
+			printf "%-2s%s " "$Sep" "$CurField"
+		fi
+	}
+
+	# Removing trailing whitespace.
+	printf "\b"
+}
+
+# Fetch kernel version.
+ProcFile='/proc/version'
+if [ -f $ProcFile -a -r $ProcFile ]; then
+	# Get version string from PROC file.
+	read -a VerStr < $ProcFile
+	CurKernVer=${VerStr[2]}
+elif type -fP uname > /dev/null 2>&1; then
+	# Resort to using an external utility.
+	CurKernVer=`uname -r`
+fi
+
+# Assign values of the current date and time to an array.
+read -a DateTime <<< "$(printf '%(%F %X)T')"
+
+# Newline escaped to keep code from trailing off-screen.
+FieldOut '|' "$USER ($UID)" "$HOSTNAME"\
+	"${CurKernVer:-N/A}" "${DateTime[0]}"  "${DateTime[1]}"
