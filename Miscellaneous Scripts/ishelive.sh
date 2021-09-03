345068a0066d5e96f837a5b2e62c3985fa8b8850
terminalforlife <terminalforlife@yahoo.com>
2021-09-03 14:18:36 +0100

    Add ishelive.sh (shows when I'm live on YouTube)
    May not be 100% reliable, but should suffice, at least for now.

---
+++ Miscellaneous Scripts/ishelive.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# Project Name      - ChannelFiles/Miscellaneous Scripts/ishelive.sh
+# Started On        - Fri  3 Sep 13:57:14 BST 2021
+# Last Change       - Fri  3 Sep 14:18:30 BST 2021
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#------------------------------------------------------------------------------
+# Features:
+#
+# N/A
+#
+# Bugs:
+#
+# N/A
+#
+# Dependencies:
+#
+#   bash (>= 4.3-14)
+#   wget (>= 1.19.4-1) | curl (>= 7.58.0-2)
+#------------------------------------------------------------------------------
+
+CurVer='2021-09-03'
+Progrm=${0##*/}
+
+Usage(){
+	while read; do
+		printf '%s\n' "$REPLY"
+	done <<-EOF
+		Usage: $Progrm [OPTS]
+
+		  -h, --help               - Display this help information.
+		  -v, --version            - Output the version datestamp.
+	EOF
+}
+
+Err(){
+	printf 'ERROR: %s\n' "$2" 1>&2
+	[ $1 -gt 0 ] && exit $1
+}
+
+Domain='https://www.youtube.com'
+URL="$Domain/results?search_query=Terminalforlife&sp=EgQQAUAB"
+
+while [ "$1" ]; do
+	case $1 in
+		--help|-h|-\?)
+			Usage; exit 0 ;;
+		--version|-v)
+			printf '%s\n' "$CurVer"; exit 0 ;;
+		*)
+			Err 1 'Incorrect option(s) specified.' ;;
+	esac
+	shift
+done
+
+if type -P wget &> /dev/null; then
+	Data=`wget -qO - "$URL"`
+elif type -P curl &> /dev/null; then
+	Data=`curl -s "$URL"`
+else
+	Err 1 "Neither 'wget' nor 'curl' found."
+fi
+
+while read Line; do
+	case $Line in
+		*'"label":"LIVE NOW"'*) IsLive='True' ;;
+	esac
+done <<< "$Data"
+
+printf 'Terminalforlife: '
+if [ "$IsLive" == 'True' ]; then
+	printf '\e[92mLive\e[0m\n'
+else
+	printf '\e[91mOffline\e[0m\n'
+fi
