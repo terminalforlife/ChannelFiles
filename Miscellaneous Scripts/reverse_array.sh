4aa43c0d5887d9ecf5fba6aa63877c8da66b93b1
terminalforlife <terminalforlife@yahoo.com>
2021-12-07 02:41:15 +0000

    Add reverse_array.sh

---
+++ Miscellaneous Scripts/reverse_array.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# Project Name      - ChannelFiles/Miscellaneous Scripts/reverse_array.sh
+# Started On        - Tue  7 Dec 02:33:55 GMT 2021
+# Last Change       - Tue  7 Dec 02:41:12 GMT 2021
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#------------------------------------------------------------------------------
+# Three ways in which you can reverse an array in pure-BASH.
+#------------------------------------------------------------------------------
+
+printf 'Reversing array of the following values:\n'
+printf '100 90 80 70 60 50 40 30 20 10\n\n'
+
+printf 'Approach 1, using `while` loop:\n'
+Array=(100 90 80 70 60 50 40 30 20 10)
+
+Nr=$((${#Array[@]} - 1))
+while (( Nr >= 0 )); do
+	printf '%d ' ${Array[Nr]}
+
+	let Nr--
+done
+
+printf '\n\n'
+
+printf 'Approach 2, using `extdebug` & `$BASH_ARGV`:\n'
+Array=(100 90 80 70 60 50 40 30 20 10)
+
+Func() {
+	Array=("${BASH_ARGV[@]}")
+}
+
+shopt -s extdebug
+Func "${Array[@]}"
+shopt -u extdebug
+
+printf '%d ' "${Array[@]}"
+
+printf '\n\n'
+
+printf 'Approach 3, using C-style `for` loop:\n'
+Array=(100 90 80 70 60 50 40 30 20 10)
+
+for (( Nr = ${#Array[@]} - 1; Nr >= 0; Nr-- )); {
+	printf '%d ' ${Array[Nr]}
+}
+
+printf '\n'
