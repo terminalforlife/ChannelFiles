384904d21782c27e5562facb0d402a158e47a0e0
terminalforlife <terminalforlife@yahoo.com>
2022-04-18 19:11:36 +0100

    Add code for the recent thumbnail changes
    Finally using the boxing! I like it, but I wish it would center, so my
    next task might be to have the box center the text, which is doable, -
    albeit a pain in the butt.

--- Miscellaneous Scripts/thumbnail-generator.sh
+++ Miscellaneous Scripts/thumbnail-generator.sh
@@ -1,43 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
 #------------------------------------------------------------------------------
 # Project Name      - ChannelFiles/Miscellaneous Scripts/thumbnail-generator.sh
 # Started On        - Thu 14 Jan 20:54:57 GMT 2021
-# Last Change       - Mon 13 Sep 11:34:06 BST 2021
+# Last Change       - Fri  8 Apr 23:28:47 BST 2022
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
 # Crappy thumbnail generator, because the people demand it!
 #------------------------------------------------------------------------------
 
-Font='Ubuntu-Mono-Bold'
-FontSize=180
+#Font='Ubuntu-Mono-Bold'
+#Font='JetBrains-Mono-Bold'
+Font='3270-Medium'
+FontSize=160
 Text=$1
 TextColor='#FFFFFF'
-StrokeColor='#000000'
-StrokeWidth=4
-Original="$HOME/Pictures/Surface Texture with Logo 2.jpg"
-Output="$HOME/Desktop/output.jpg"
+Original="$HOME/Pictures/TFL Banner (simple_1080p).jpg"
+Output="$HOME/Desktop/thumbnail.jpg"
 
 # Use this if you want multi-line auto-boxing of the text. This needs BASH.
-#Title() {
-#	MaxLen=0
-#	IFS='|' read -a Lines <<< "$1"
-#	for Line in "${Lines[@]}"; {
-#		Len=${#Line}
-#		(( Len > MaxLen )) && MaxLen=$Len
-#	}
-#
-#	printf -v Bar '%*s' $((MaxLen + 2)) ' '
-#
-#	printf '%s\n' "┌${Bar// /─}┐"
-#	for Line in "${Lines[@]}"; {
-#		printf '│ %-*s │\n' $MaxLen "$Line"
-#	}
-#	printf '%s\n' "└${Bar// /─}┘"
-#}
-#
-#printf -v Text '%b' "$(Title "$1")"
+Title() {
+	# Similar to above's `for` loop, but interpreting a line delimiter of '|'.
+	IFS='|' read -ra Lines <<< "$1"
+	for Line in "${Lines[@]}"; {
+		Len=${#Line}
+		(( Len > MaxLen )) && MaxLen=$Len
+	}
+
+	printf -v Bar '%*s' $((MaxLen + 0)) ' '
+
+	printf '%s\n' "⌜${Bar// / }⌝"
+	for Line in "${Lines[@]}"; {
+		printf ' %*s \n' $MaxLen "$Line"
+	}
+	printf '%s\n' "⌞${Bar// / }⌟"
+}
+
+printf -v Title '%s' "$(Title "$1")"
+printf '%s\n' "$(Title "$1")"
+
+#exit
 
 Err(){
 	printf 'ERROR: %s\n' "$2" 1>&2
@@ -49,14 +52,12 @@ Err(){
 command -v convert 1> /dev/null 2>&1 || Err 1 "Dependency 'convert' not met."
 command -v feh 1> /dev/null 2>&1 || Err 1 "Dependency 'feh' not met."
 
-convert "$Original" -background '#000000' -vignette 0x120+-60-60\
-	-quality 100 "$Output"
+convert "$Original" -gaussian-blur 18x18 -quality 100 "$Output"
 
 [ $? -eq 0 ] && Err=$((Err + $?))
 
-convert "$Output" -fill "$TextColor" -stroke "$StrokeColor"\
-	-strokewidth $StrokeWidth -pointsize "$FontSize" -font "$Font"\
-	-gravity Center -annotate +0 "$Text" -quality 100 "$Output"
+convert "$Output" -fill "$TextColor" -pointsize "$FontSize" -font "$Font"\
+	-weight Medium -gravity Center -annotate +0 "$Title" -quality 100 "$Output"
 
 [ $? -eq 0 ] && Err=$((Err + $?))
 
